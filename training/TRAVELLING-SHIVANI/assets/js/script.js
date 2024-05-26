06a5379bf04509fd4571687c7b44c6f3db371592
Shivani Yadav <155175405+shivaniyadav-2003@users.noreply.github.com>
2024-05-26 22:51:03 +0530

    Add files via upload

---
+++ training/TRAVELLING-SHIVANI/assets/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500 ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
